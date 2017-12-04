1467ebbd8d918d8124115367de4cd7c41c8797e0
lidekai <2665252078@qq.com>
2017-12-04 17:58:24 +0800

    给项目的页面添加数据格式

--- js/project/project-service.js
+++ js/project/project-service.js
@@ -1,6 +1,60 @@
 (function($){
 	$.extend({
-		getProjectData:function(){
+		getProjectData:function(){//表格
+			var getDiv = document.getElementById( "dataTableWorkflow");
+            for(var i=0;i<getWorkflowData.length;i++){
+		            	var createTr =document.createElement("tr");
+		            	createTr.setAttribute("class","gradeX");
+		            	createTr.setAttribute("value",getWorkflowData[i].workflowId);
+
+		            	var createTdFirst=document.createElement("td");
+		            	createTdFirst.setAttribute("class","text-center");
+		            	createTdFirst.innerHTML=getWorkflowData[i].workflowName;
+		            	createTr.appendChild(createTdFirst);
+
+		            	var createTdSecond =document.createElement("td");
+		            	createTdSecond.setAttribute("class","text-center");
+		            	createTdSecond.innerHTML=getWorkflowData[i].node.length;
+		            	createTr.appendChild(createTdSecond);
+
+		            	var createTdThird =document.createElement("td");
+		            	createTdThird.setAttribute("class","text-center");
+
+		            	var creatButtonFirst =document.createElement("button");
+		            	creatButtonFirst.setAttribute("class","btn btn-info btn-xs editWorkflow");
+		            	createTdThird.appendChild(creatButtonFirst);
+
+		            	var creatiFirst =document.createElement("i");
+		            	creatiFirst.setAttribute("class","fa fa-pencil-square");
+		            	creatiFirst.innerHTML="&nbsp;&nbsp;修改";
+		            	creatButtonFirst.appendChild(creatiFirst);
+
+
+		            	var creatButtonSecond =document.createElement("button");
+		            	creatButtonSecond.setAttribute("class","btn btn-success btn-xs eyeWorkflow");
+		            	createTdThird.appendChild(creatButtonSecond);
+
+		            	var creatiSecond =document.createElement("i");
+		            	creatiSecond.setAttribute("class","fa fa-eye");
+		            	creatiSecond.setAttribute("data-toggle","modal");
+		            	creatiSecond.setAttribute("data-target","#eyeWorkflow");
+		            	creatiSecond.innerHTML="&nbsp;&nbsp;查看";
+		            	creatButtonSecond.appendChild(creatiSecond);
+
+
+		            	var creatButtonThird =document.createElement("button");
+		            	creatButtonThird.setAttribute("class","btn btn-danger btn-xs delectWorkflow");
+		            	createTdThird.appendChild(creatButtonThird);
+
+		            	var creatiThird =document.createElement("i");
+		            	creatiThird.setAttribute("class","fa fa-trash");
+		            	creatiThird.innerHTML="&nbsp;&nbsp;删除";
+		            	creatButtonThird.appendChild(creatiThird);
+
+		            	createTr.appendChild(createTdThird);
+
+		            	getDiv.appendChild(createTr);
+            }
 			$('.dataTables-example').DataTable({
                 pageLength: 25,
                 responsive: true,
